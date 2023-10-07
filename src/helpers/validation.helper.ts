bb38cd27be257ae459dc3a01007bd6057e8ac403
alif-arrizqy <alif.arrizqy20@gmail.com>
2023-10-07 21:25:06 +0700

    create validation date range

--- src/helpers/validation.helper.ts
+++ src/helpers/validation.helper.ts
@@ -18,6 +18,20 @@ class DateValidation {
 			};
 		}
 	}
+
+	async rangeDate(startDate: string, endDate:string) {
+		const start = new Date(startDate);
+		const end = new Date(endDate);
+		const range = Math.abs(end.getTime() - start.getTime());
+		const days = Math.ceil(range / (1000 * 3600 * 24));
+		if (days > 1) {
+			return {
+				code: 400,
+				success: false,
+				message: "range date can not be greater than 1 days!",
+			};
+		}
+	}
 }
 
 export default new DateValidation();
