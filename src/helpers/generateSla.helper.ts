cdee9f6a6c868504a325e03e04f7512d4874e855
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-02-06 10:26:09 +0700

    feat: Enhance SLA generation with site categorization and detailed messaging

--- src/helpers/generateSla.helper.ts
+++ src/helpers/generateSla.helper.ts
@@ -100,116 +100,184 @@ class GenerateSLA {
 
 	generateDetail = (dates: string[], data: Sla[]): Promise<Sla[]> => {
 		return new Promise(async (resolve, reject) => {
+			// Define site categories
+			const siteCategories: any = {
+					talis5: { message: "Sundaya TALIS", downSla: [], underSla: [], dropSla: [], upSla: [] },
+					mix: { message: "Sundaya MIX", downSla: [], underSla: [], dropSla: [], upSla: [] },
+					jspro: { message: "Sundaya JSPro", downSla: [], underSla: [], dropSla: [], upSla: [] },
+			};
+
 			const downSla: any[] = [];
 			const underSla: any[] = [];
 			const dropSla: any[] = [];
 			const upSla: any[] = [];
 			const newDownSla: any[] = [];
+			
+			const totalSiteTalis: any[] = [];
+			const totalSiteMix: any[] = [];
+			const totalSiteJspro: any[] = [];
 
-			for (let index = 0; index < dates.length; index++) {
-				const newSla: Sla[] = [];
+			const slaTalis: any[] = [];
+			const slaMix: any[] = [];
+			const slaJspro: any[] = [];
+			
+			const diffTalis: number[] = [];
+			const diffMix: number[] = [];
+			const diffJspro: number[] = [];
 
-				data.forEach((item) => {
-					if (dates[index] === item.date?.toString()) {
-						const sla: number =
-							(item.sla as number) < 1
-								? parseFloat(((item.sla as number) * 100).toFixed(2))
-								: (item.sla as number) * 100;
-						item.sla = sla;
-						newSla.push(item);
+			let messageTalis: string = "";
+			let messageMix: string = "";
+			let messageJspro: string = "";
+
+		for (let index = 0; index < dates.length; index++) {
+			const newSla: Sla[] = data.filter(item => dates[index] === item.date?.toString())
+					.map(item => {
+							item.sla = (item.sla as number) < 1 ? parseFloat(((item.sla as number) * 100).toFixed(2)) : (item.sla as number) * 100;
+							return item;
+					});
+
+			newSla.forEach(item => {
+					if (item.battery_version === "talis5") slaTalis.push(item);
+					else if (item.battery_version === "mix") slaMix.push(item);
+					else if (item.battery_version === "jspro") slaJspro.push(item);
+			});
+
+			// Calculate average SLA
+			const calculateAverageSla = (slaArray: any[], diffArray: number[]) => {
+					const sum = slaArray.reduce((a, b) => a + (b.sla as number), 0);
+					const avg = sum / slaArray.length;
+					diffArray.push(avg);
+					return avg;
+			};
+
+			const avgTalis = calculateAverageSla(slaTalis, diffTalis);
+			const avgMix = calculateAverageSla(slaMix, diffMix);
+			const avgJspro = calculateAverageSla(slaJspro, diffJspro);
+
+			// Calculate difference SLA
+			const calculateDifference = (diffArray: number[]) => {
+					if (diffArray.length > 1) {
+							return parseFloat((diffArray[diffArray.length - 1] - diffArray[diffArray.length - 2]).toFixed(2));
 					}
-				});
+					return 0;
+			};
 
-				// check if sla < 95.5 in last of dates
-				if (index === dates.length - 1) {
-					newSla.forEach((item) => {
+			const valueDiffTalis = calculateDifference(diffTalis);
+			const valueDiffMix = calculateDifference(diffMix);
+			const valueDiffJspro = calculateDifference(diffJspro);
+
+			const generateMessage = (valueDiff: number, type: string) => {
+					return valueDiff < 0
+							? `Terdapat penurunan sebesar ${Math.abs(valueDiff)} % dari periode sebelumnya`
+							: `Terdapat kenaikan sebesar ${Math.abs(valueDiff)} % dari periode sebelumnya`;
+			};
+
+			messageTalis = generateMessage(valueDiffTalis, "Talis");
+			messageMix = generateMessage(valueDiffMix, "Mix");
+			messageJspro = generateMessage(valueDiffJspro, "Jspro");
+
+			// Check if SLA < 95.5 in last date
+			if (index === dates.length - 1) {
+				newSla.forEach(item => {
+						if (item.battery_version === "talis5") totalSiteTalis.push(item.sites);
+						if (item.battery_version === "mix") totalSiteMix.push(item.sites);
+						if (item.battery_version === "jspro") totalSiteJspro.push(item.sites);
+
 						if ((item.sla as number) < 95.5 && (item.sla as number) > 0) {
-							underSla.push({
-								date: item.date,
-								sla: item.sla,
-								downtime: item.downtime_percent,
-								site: item.sites,
-							});
+								underSla.push({
+										date: item.date,
+										sla: item.sla,
+										downtime: item.downtime_percent,
+										site: item.sites,
+										battery_version: item.battery_version,
+								});
 						}
 						if ((item.sla as number) === 0) {
-							downSla.push({
-								date: item.date,
-								sla: item.sla,
-								downtime: item.downtime_percent,
-								site: item.sites,
-							});
-						}
-					});
-				}
-
-				const results = await Promise.all(
-					downSla.map(async (item) => {
-						const weekDate = getPreviousWeeksDate(item.date);
-						const countDowntime = await slaRepository.getCountDowntime(
-							weekDate,
-							item.date,
-							item.site
-						);
-						item.downtime = countDowntime > 7 ? "Lebih Dari 7 Hari" : `${countDowntime} Hari`;
-						return item;
-					})
-				);
-				newDownSla.push(...results);
-
-				// add times description in underSla using format time
-				underSla.forEach((item) => {
-					const desc = generateTimeFormat(item.downtime);
-					Object.assign(item, { downtime: desc });
-				});
-
-				const separateByDate: any[] = [];
-				newSla.forEach((item) => {
-					if (dates[index] === item.date?.toString()) {
-						separateByDate.push(item);
-					}
-				});
-
-				if (index > 0) {
-					const tempBefore: any[] = [];
-					data.forEach((item) => {
-						if (dates[index - 1] === item.date?.toString()) {
-							tempBefore.push(item);
-						}
-					});
-
-					tempBefore.forEach((item) => {
-						separateByDate.forEach((element) => {
-							if (item.sites === element.sites) {
-								if (item.sla > 95.5 && element.sla > 95.5) {
-									// skip
-								} else if (item.sla > element.sla) {
-									dropSla.push({
-										date: element.date,
+								downSla.push({
+										date: item.date,
+										sla: item.sla,
+										downtime: item.downtime_percent,
 										site: item.sites,
-										slaBefore: item.sla,
-										slaNow: element.sla,
-									});
-								} else if (item.sla < element.sla) {
-									upSla.push({
-										date: element.date,
-										site: item.sites,
-										slaBefore: item.sla,
-										slaNow: element.sla,
-									});
-								}
-							}
-						});
-					});
-				}
+										battery_version: item.battery_version,
+								});
+						}
+				});
 			}
 
-			const sla: any = {
-				downSla: newDownSla,
-				underSla: underSla,
-				dropSla: dropSla,
-				upSla: upSla,
-			};
-			resolve(sla);
+			// Add downtime in downSla
+			const results = await Promise.all(
+					downSla.map(async item => {
+							const weekDate = getPreviousWeeksDate(item.date);
+							const countDowntime = await slaRepository.getCountDowntime(weekDate, item.date, item.site);
+							item.downtime = countDowntime > 7 ? "Lebih Dari 7 Hari" : `${countDowntime} Hari`;
+							return item;
+					})
+			);
+			newDownSla.push(...results);
+
+			// Add time information in underSla
+			underSla.forEach(item => {
+					const desc = generateTimeFormat(item.downtime);
+					Object.assign(item, { downtime: desc });
+			});
+
+			// Add SLA Before and SLA Now in dropSla and upSla
+			const separateByDate = newSla.filter(item => dates[index] === item.date?.toString());
+
+			if (index > 0) {
+				const tempBefore = data.filter(item => dates[index - 1] === item.date?.toString());
+
+				tempBefore.forEach(item => {
+					separateByDate.forEach(element => {
+						if (item.sites === element.sites) {
+							if ((item.sla ?? 0) > 95.5 && (element.sla ?? 0) > 95.5) return;
+							if ((item.sla ?? 0) > (element.sla ?? 0)) {
+									dropSla.push({
+											date: element.date,
+											site: item.sites,
+											slaBefore: item.sla,
+											slaNow: element.sla,
+									});
+							} else if ((item.sla ?? 0) < (element.sla ?? 0)) {
+									upSla.push({
+											date: element.date,
+											site: item.sites,
+											slaBefore: (item.sla ?? 0),
+											slaNow: element.sla,
+									});
+							}
+						}
+					});
+				});
+			}
+		}
+
+		const totalTalis = new Set(totalSiteTalis).size;
+		const totalMix = new Set(totalSiteMix).size;
+		const totalJspro = new Set(totalSiteJspro).size;
+
+		siteCategories.talis5.message = `Sundaya TALIS (${totalTalis} Site) pada tanggal ${dates[1]} ${messageTalis}`;
+		siteCategories.mix.message = `Sundaya MIX (${totalMix} Site) pada tanggal ${dates[1]} ${messageMix}`;
+		siteCategories.jspro.message = `Sundaya JSPro (${totalJspro} Site) pada tanggal ${dates[1]} ${messageJspro}`;
+
+		const filterByBatteryVersion = (array: any[], version: string) => array.filter(item => item.battery_version === version);
+
+		siteCategories.talis5.downSla = filterByBatteryVersion(newDownSla, "talis5");
+		siteCategories.talis5.underSla = filterByBatteryVersion(underSla, "talis5");
+		siteCategories.talis5.dropSla = filterByBatteryVersion(dropSla, "talis5");
+		siteCategories.talis5.upSla = filterByBatteryVersion(upSla, "talis5");
+
+		siteCategories.mix.downSla = filterByBatteryVersion(newDownSla, "mix");
+		siteCategories.mix.underSla = filterByBatteryVersion(underSla, "mix");
+		siteCategories.mix.dropSla = filterByBatteryVersion(dropSla, "mix");
+		siteCategories.mix.upSla = filterByBatteryVersion(upSla, "mix");
+
+		siteCategories.jspro.downSla = filterByBatteryVersion(newDownSla, "jspro");
+		siteCategories.jspro.underSla = filterByBatteryVersion(underSla, "jspro");
+		siteCategories.jspro.dropSla = filterByBatteryVersion(dropSla, "jspro");
+		siteCategories.jspro.upSla = filterByBatteryVersion(upSla, "jspro");
+
+		resolve(siteCategories);
 		});
 	};
 
